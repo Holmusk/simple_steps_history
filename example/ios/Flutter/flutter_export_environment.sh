3f2c9f425393c79b92b7191bc404521f89620cfb
Daniel Tjuatja <daniel.tjuatja@gmail.com>
2020-01-16 15:15:13 +0800

    bump up intl version

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/danieltjuatja/Repositories/vendors/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/danieltjuatja/Repositories/Holmusk/simple_steps_history/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/danieltjuatja/Repositories/vendors/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
